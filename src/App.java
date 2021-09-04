448e47b2c217a515152c7a29b40b5de370a47f02
Dev-LiamKim <johnbhjbg@gmail.com>
2021-09-04 13:42:29 +0000

    Quadrant_n Solved

--- src/App.java
+++
@@ -1,7 +0,0 @@
-
-
-public class App{
-    public static void main(String[] args){
-        Quadrant_n solution = new Quadrant_n();
-    }
-}
--- src/Quadrant_n.java
+++ src/Quadrant_n.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Quadrant_n{
-    public void Solution(){
-        
+// Quadrant_n.java
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int p;
+        if (x > 0) {
+            if (y > 0)
+                p = 1;
+            else {
+                p = 4;
+            }
+        } else {
+            if (y > 0)
+                p = 2;
+            else {
+                p = 3;
+            }
+        }
+        System.out.println(p);
     }
 }
